b08bb3cb7c5ec572190d3110de8f98e4acac076e
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-11-26 21:48:47 +0530

    Add dataframe class

---
+++ libs/berkelium/src/lib/modules/dataframe.ts
@@ -0,0 +1,20 @@
+export class DataFrame {
+    private data: Array<object>;
+
+    /**
+   * Creates a DataFrame object.
+   * @param {Array<Object>} data - Array of objects where keys are column names.
+   */
+    constructor(data: Array<object>) {
+        this.data = data;
+    }
+
+    /**
+   * Displays the first n rows of the DataFrame.
+   * @param {number} n - Number of rows to display (default: 5).
+   * @returns {Array<Object>} - The first n rows.
+   */
+    head(n = 5) {
+        return this.data.slice(0, n);
+    }
+}
