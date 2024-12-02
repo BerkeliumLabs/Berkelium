f9f397f8de9eecae5fe5690890426eff3f90d05c
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-12-02 19:21:02 +0530

    Add JSDoc to DataReader

--- libs/berkelium/src/lib/util/datareader.ts
+++ libs/berkelium/src/lib/util/datareader.ts
@@ -1,4 +1,12 @@
 export class DataReader {
+    /**
+     * Parses a CSV formatted string into an array of objects.
+     * Each object represents a row in the CSV data, with keys derived from the header row.
+     * 
+     * @param {string} csvData - The CSV data as a string.
+     * @param {string} [delimiter=','] - The delimiter used to separate values in the CSV data.
+     * @returns {Array<object>} An array of objects where each object corresponds to a row in the CSV data.
+     */
     readCSV(csvData: string, delimiter = ',') {
         // Split the data into rows
         const rows = csvData.trim().split("\n");
